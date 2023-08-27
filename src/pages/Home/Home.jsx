6761b71758450caae55bda080cee8604e601d412
Jacob Samuel <imjacobsamuel@gmail.com>
2023-08-27 17:18:11 +0530

    Top Rated and Popular completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import "./style.scss";
 import HeroBanner from "./HeroBanner/HeroBanner";
 import Trending from "./Trending/Trending";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
       <Trending />
+      <Popular />
+      <TopRated />
       <div style={{ height: "1000px" }}></div>
     </div>
   );
